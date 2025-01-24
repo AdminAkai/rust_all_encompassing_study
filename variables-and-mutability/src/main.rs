cab3e78acbdc38acfea214f73903b905807683d4
AdminAkai <akatoirotech@gmail.com>
2025-01-24 00:37:05 -0500

    working on Rust syntax

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,7 +1,34 @@
 fn main() {
+  // Variables
   let apples = 50;
   let oranges = 14 + 6;
   let fruits = apples + oranges;
 
   println!("This year, my garden has {fruits} fruits.");
+
+  // Mutable Variable
+  let mut gym_reps = 10;
+  println!("I plan to do {gym_reps} reps.");
+
+  gym_reps = 5;
+  println!("I plan to do {gym_reps} reps.");
+
+  // Variable Shadowing
+  let grams_of_protein = "100.345";
+  println!("I ate {grams_of_protein} is a string.");
+
+  let grams_of_protein = 100.345;
+  println!("I ate {grams_of_protein} is f64.");
+
+  let grams_of_protein = 100;
+  println!("I ate {grams_of_protein} is i32.");
+
+  // Scopes/Blocks
+  let coffee_price = 5.99;
+
+  {
+    let cookie_price = 1.99;
+    println!("The price of cookies is {cookie_price}.");
+    println!("The price of coffee is {coffee_price}.");
+  }
 }
